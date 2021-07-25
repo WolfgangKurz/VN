22b3bdf85a3cac7f2e7bbc0a1fa145cdfdeeaef3
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2021-07-26 00:48:00 +0900

    VNStatus에서 VNCode 및 VNCodeType 분리

---
+++ VNScript/VNCodeType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VN.VNScript {
+	internal enum VNCodeType : int {
+		NONE = 0,
+		SCRIPT,
+		SET,
+		UNLOCK,
+		TEXT,
+		SAY,
+		SEL,
+		BGM,
+		BG,
+		SCG,
+		FX,
+		WAIT,
+	}
+}
