f41bdd39207e1db0320cf4b33b58cc29f31b729b
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2021-12-16 16:34:02 +0900

    Sprite 툴 개선

--- Tool/texture.js
+++ Tool/texture.js
@@ -2,8 +2,10 @@ const SpriteSmith = require("spritesmith");
 const path = require("path");
 const fs = require("fs");
 
-const baseDir = path.resolve(__dirname, "..", "res", "ui");
+const baseDir = path.resolve(__dirname, "..", "res", "리소스", "03_컬렉션");
 const sprites = fs.readdirSync(baseDir)
+	.filter(x => x.startsWith("collect"))
+	.filter(x => !x.endsWith("back.png"))
 	.map(x => path.resolve(baseDir, x));
 
 SpriteSmith.run(
@@ -25,8 +27,21 @@ SpriteSmith.run(
 			"utf-8",
 		);
 		fs.writeFileSync(
-			path.resolve(__dirname, "output.png"),
+			path.resolve(__dirname, "output.sprite.png"),
 			result.image
+		);
+
+		const ret = [];
+		Object.keys(table)
+			.forEach(k => {
+				const e = table[k];
+				ret.push(`${k}\t${e.x}\t${e.y}\t${e.width}\t${e.height}`);
+			});
+
+		fs.writeFileSync(
+			path.resolve(__dirname, "output.sprite.txt"),
+			ret.join("\n"),
+			"utf-8",
 		);
 	},
 );
