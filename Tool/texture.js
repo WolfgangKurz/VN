771fed758382cf22423be58b9ad372f10efb6718
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2023-01-18 12:48:01 +0900

    atlas tool

--- Tool/texture.js
+++ Tool/texture.js
@@ -1,28 +1,18 @@
-const os = require("os");
-os.tmpDir = os.tmpdir;
+const cp = require("child_process");
 
-const spritesheet = require("spritesheet-js");
-const glob = require("glob");
 const path = require("path");
 const fs = require("fs");
+const rimraf = require("rimraf");
 
-// const baseDir = path.resolve(__dirname, "..", "res", "리소스", "03_컬렉션");
-// const sprites = fs.readdirSync(baseDir)
-// 	.filter(x => x.startsWith("collect"))
-// 	.filter(x => !x.endsWith("back.png"))
-// 	.map(x => path.resolve(baseDir, x));
-const sprites = glob.sync(path.resolve(__dirname, "..", "res", "temp", "*.png"));
-spritesheet(
-	sprites,
-	{
-		format: "pixi.js",
-		path: "./output",
-		name: "sprite",
-		trim: true,
-	},
-	function (err) {
-		if (err) throw err;
+fs.readdirSync(path.join(__dirname, "input"))
+	.forEach(dir => {
+		const outDir = path.join(__dirname, "output", dir);
+		if (fs.existsSync(outDir))
+			rimraf.sync(outDir);
 
-		console.log("spritesheet successfully generated");
-	},
-);
+		fs.mkdirSync(outDir, { recursive: true });
+
+		cp.execSync(`python atlas.py "./input/${dir}" "./output/${dir}/sprite.json"`);
+	});
+
+console.log("spritesheet successfully generated");
